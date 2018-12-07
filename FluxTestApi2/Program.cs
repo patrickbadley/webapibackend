542a0cacc4172f16cd2586bf32cd6a934194706b
Patrick Badley <patrickbadley@gmail.com>
2018-12-07 15:49:21 -0500

    initital commit

---
+++ FluxTestApi2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FluxTestApi2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    options.ListenAnyIP(5000);
+                    options.ListenAnyIP(5001, listenOptions =>
+                    {
+                        listenOptions.UseHttps("cert.pfx", "1234");
+                    });
+                })
+            .UseUrls("https://+;http://+");
+    }
+}
