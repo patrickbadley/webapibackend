5ec4fd4751a25dbd285b666348ec3e59a39b821a
Patrick Badley <pjb2@hmbnet.com>
2019-02-04 14:46:29 -0500

    cleaning up name and adding new variable to allow for different image and project names.

--- Api/Controllers/ValuesController.cs
+++ Api/Controllers/ValuesController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MyWebApi.Controllers
+namespace Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MyWebApi
+namespace Api
 {
     public class Startup
     {
